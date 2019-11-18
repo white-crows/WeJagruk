a2252aff8e04f44becb11e1e3c7e4dcb8f8c803b
rajatsrma <rajatsharma1203@gmail.com>
2019-11-18 16:56:57 +0530

    addissue functionality added

---
+++ frontend/addIssue.js
@@ -0,0 +1,8 @@
+document.getElementById("issueForm").addEventListener("submit", changeLoc);
+
+//perform signup request---------------
+function changeLoc(e) {
+  //   alert("Your response has been succesfully registered");
+  //   window.location.href = "feed.html";
+  //   e.preventDefault();
+}
